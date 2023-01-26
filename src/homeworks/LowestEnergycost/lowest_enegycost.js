80b1a78780ae5ca64b7d13279b6c3d12c2f7dd61
RED-MHWK <116504217+RED-MHWK@users.noreply.github.com>
2023-01-26 21:01:42 +0100

    Homework EEX (finished?(unfortunately I struggle with Jest to work, therefore I only tested everything over console, therefore couldn't test the testcase part of the homework(had help for the NaN Problem)

--- src/homeworks/LowestEnergycost/lowest_enegycost.js
+++ src/homeworks/LowestEnergycost/lowest_enegycost.js
@@ -1,91 +1,28 @@
-/*
-Note
-we got 24 values in an order
-0 = 100.00
-1 = 100.00
-2 = 100.00
-3 = 200.00
-4 = 200.00
-5 = 200.00
-6 = 250.00
-7 = 250.00
-8 = 400.00
-9 = 400.00
-10 = 400.00
-11 = 320.00
-12 = 320.00
-13 = 320.00
-14 = 320.00
-15 = 360.00
-16 = 360.00
-17 = 360.00
-18 = 300.00
-19 = 300.00
-20 = 300.00
-21 = 300.00
-22 = 150.00
-23 = -10.000
- */
 
 const pricePreview = [100.00, 100.00, 100.00, 200.00, 200.00, 200.00, 250.00, 250.00, 400.00, 400.0, 400.00, 320.00, 320.00, 320.00, 320.00, 360.00, 360.00, 360.00, 300.00, 300.00, 300.00, 300.00, 150.00, -10.000]
 
-
-function priceComparison(hoursNeeded) {
-const cheapest = pricePreview.indexOf(Math.min(...pricePreview));
-const indexPos = cheapest + hoursNeeded;
-const currentConc = [];
-if(hoursNeeded >= 24 || hoursNeeded <= 0){
-    throw new Error('Hours needed for the process must be within the range 24 hours, therefore more than 0 and less than 24.');
-    }
-if(hoursNeeded >= 4 && hoursneeded <= 23){
-    throw new Error('Sorry, these values are not considered for now...')
-}
-else{
-    if (indexPos === pricePreview.length) {
-        return cheapest;
-    } else {
-    priceCompSt(hoursNeeded)
-    }
-}
+function pricecomparison(pricesConc) {
+    let filteredConc = pricesConc.filter(val => !isNaN(val));
+    let solution = Math.min(...filteredConc);
+    return pricesConc.indexOf(solution);
 }
 
-function priceCompSt(hoursNeeded){
-    if(hoursNeeded === 2) {
-        const [first, second, ...rest] = pricePreview;
-        const current = (first + second) / hoursNeeded;
-        for (let i = 0; i < rest.length; i++) {
-            let value = rest[i];
-            if (current < value) {
-                let windowedCheapest = pricePreview.indexOf(first)
-                return windowedCheapest;
-            }
-        }
+function priceCalc(hoursNeeded) {
+    if (hoursNeeded <= 0 || hoursNeeded >= 24){
+        throw new Error('The time your process needs got to be between 0 and 24 hours.')
     }
     else{
-        let pricePreview = rest;
-        priceCompSt(pricePreview);
-    }
-    if(hoursNeeded === 3) {
-        const [first, second, third, ...rest] = pricePreview;
-        const current = (first + second + third) / hoursNeeded;
-        for(let i = 0; i < rest.length; i++){
-            let value = rest[i];
-            if(current < value){
-                let windowedCheapest = pricePreview.indexOf(first)
-                return windowedCheapest;
+        let pricesConc =[];
+        for(let i = 0; i < pricePreview.length; i++){
+            let value = 0;
+            for(let j = i; j < i + hoursNeeded; j++){
+                value += pricePreview[j];
             }
+            pricesConc.push(value);
         }
-    }
-    else{
-        let pricePreview = rest;
-        priceCompSt(pricePreview);
+    return pricecomparison(pricesConc);
     }
 }
 
 
-/*
-
- */
-
-console.log(priceComparison(2))
-export {priceComparison}
+export{priceCalc}
--- src/homeworks/LowestEnergycost/lowest_energycost.test.js
+++ src/homeworks/LowestEnergycost/lowest_energycost.test.js
@@ -1,18 +1,22 @@
 
-import {priceComparison} from "./lowest_enegycost.js";
+import {priceCalc} from "./lowest_enegycost.js";
 
 test('Timeframe of 1 Hour', (hoursNeeded) => {
     expect(priceComparison(1)).toBe(23)
 })
+
 test('Timeframe of 2 Hour', (hoursNeeded) => {
     expect(priceComparison(2)).toBe(22)
 })
-test('Timeframe of 2 Hour', (hoursNeeded) => {
+
+test('Timeframe of 3 Hour', (hoursNeeded) => {
     expect(priceComparison(3)).toBe(0)
 })
+
 test('Timeframe of 24 Hour', (hoursNeeded) => {
     expect(priceComparison(24)).toThrow(Error)
 })
+
 test('Timeframe of 0 Hour', (hoursNeeded) => {
     expect(priceComparison(0)).toThrow(Error)
 })
